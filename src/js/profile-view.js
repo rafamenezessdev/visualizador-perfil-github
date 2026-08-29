207b4c70028bbb613d00df2ebb8df4482b42b774
Rafael Menezes <rafamenezesscad@gmail.com>
2026-08-29 13:45:32 -0300

    verificação caso não tenha nome cadastrado

--- src/js/profile-view.js
+++ src/js/profile-view.js
@@ -25,7 +25,7 @@ export function renderProfile(userData, userRepos, container) {
     <div class="profile-card">
         <img src="${userData.avatar_url}" alt="Avatar de ${userData.name}" class="profile-avatar">
         <div class="profile-info">
-            <h2>${userData.name}</h2>
+            <h2>${userData.name || "Não possui nome cadastrado 😒"}</h2>
             <p>${userData.bio || "Não possui bio cadastrada 😒"}</p>
         </div>
     </div>
